518d35eeccf0b4061770d18f1133bd5a93ef5b25
EmnaBoukhr <139135684+EmnaBoukhr@users.noreply.github.com>
2023-12-12 21:33:05 +0100

    Update MongoDemoApplication.java

--- src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
+++ src/main/java/com/gte3/mongodemo/MongoDemoApplication.java
@@ -8,10 +8,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 class MongoDemoApplication {
 
-    @GetMapping
-	public String message(){
-		return "welcome to devops project";
-	}
 
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApplication.class, args);
